c05a082dc807f8472cc4165b4474c3e057b2487c
doxh23 <adrien.peters95@gmail.com>
2023-09-28 13:40:59 +0200

    change font size for mobile device

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ReactFancyBox from 'react-fancybox';
+import 'react-fancybox/lib/fancybox.css';
 
-export const Navbar = ({route}) => {
-  console.log(route)
+export const Navbar = ({ route }) => {
+  console.log(route);
   return (
     <>
-    <div className='navBar'>
-      
-    <ul>
-       {route.map(route => (
-        <li key={route.name}><Link to={route.path} >{route.name}</Link></li>
-
-       ))}
-    </ul>
-   
-    </div>
+      <div className="navBar">
+        <ul>
+          <li>
+            <a href="assets\CV Adrien Péters.pdf" download>
+              {' '}
+              CV
+            </a>
+          </li>
+          {route.map((route) => (
+            <li key={route.name}>
+              <Link to={route.path}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
